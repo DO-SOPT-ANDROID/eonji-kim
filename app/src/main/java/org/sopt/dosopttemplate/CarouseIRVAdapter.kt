4220380aa46845a87c2172f072bb14e0fad0362c
Eonji-sw <ejkim0625@gmail.com>
2023-11-10 01:20:53 +0900

    [feat]: viewpager2 adapter 구현

---
+++ app/src/main/java/org/sopt/dosopttemplate/CarouseIRVAdapter.kt
@@ -0,0 +1,39 @@
+package org.sopt.dosopttemplate
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import org.sopt.dosopttemplate.databinding.ItemPartBinding
+
+class CarouseIRVAdapter(private val carouselDataList: ArrayList<String>) :
+    RecyclerView.Adapter<CarouseIRVAdapter.CarouselItemViewHolder>() {
+
+    interface OnItemClickListener {
+        fun onItemClick(item: String)
+    }
+
+    var onItemClickListener: OnItemClickListener? = null
+
+    class CarouselItemViewHolder(val binding: ItemPartBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CarouselItemViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemPartBinding.inflate(inflater, parent, false)
+        return CarouselItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CarouselItemViewHolder, position: Int) {
+        val textView = holder.binding.tvPart
+        textView.clipToOutline = true
+        textView.text = carouselDataList[position]
+
+        holder.itemView.setOnClickListener {
+            onItemClickListener?.onItemClick(carouselDataList[position])
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return carouselDataList.size
+    }
+}
