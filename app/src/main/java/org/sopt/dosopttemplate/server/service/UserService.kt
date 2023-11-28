4433853f9b6ad261097786b79427dd5576f4773d
Eonji-sw <ejkim0625@gmail.com>
2023-11-28 17:01:24 +0900

    [feat]: 코루틴 사용

--- app/src/main/java/org/sopt/dosopttemplate/server/service/UserService.kt
+++ app/src/main/java/org/sopt/dosopttemplate/server/service/UserService.kt
@@ -1,13 +1,13 @@
 package org.sopt.dosopttemplate.server.service
 
 import org.sopt.dosopttemplate.server.auth.response.ResponseUserListDto
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface UserService {
     @GET("api/users")
-    fun getUserList(
+    suspend fun getUserList(
         @Query("page") page: Int
-    ): Call<ResponseUserListDto>
+    ): Response<ResponseUserListDto>
 }
